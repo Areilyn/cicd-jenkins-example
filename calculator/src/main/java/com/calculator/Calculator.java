5a6f8e6457005342f77f7250de03e0dbe9e126e4
Areilyn <areilynart@gmail.com>
2025-08-08 10:04:33 +0300

    initialization

---
+++ calculator/src/main/java/com/calculator/Calculator.java
@@ -0,0 +1,76 @@
+package com.calculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Calculator {
+
+    private static final int PORT = 5000;
+
+    public static void main(String[] args) throws IOException {
+
+        try (ServerSocket svSoc = new ServerSocket(PORT)) {
+
+            while (true) {
+
+                try (Socket cliSoc = svSoc.accept()) {
+                    System.out.println("Connected through port " + PORT);
+
+                    InputStream in = cliSoc.getInputStream();
+                    OutputStream out = cliSoc.getOutputStream();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    PrintWriter writer = new PrintWriter(out, true);
+
+                    String inst;
+                    int result = 0;
+
+                    while ((inst = reader.readLine()) != null) {
+
+                        int[] arr = Arrays.stream(inst.split(" "))
+                                        .mapToInt(Integer::parseInt)
+                                        .toArray();
+                        
+                        String op = "";
+                        switch (arr[0]) {
+                            case 1:
+                                op = "+";
+                                result = arr[1] + arr[2];
+                                break;
+                            case 2:
+                                op = "-";
+                                result = arr[1] - arr[2];
+                                break;
+                            case 3:
+                                op = "x";
+                                result = arr[1] * arr[2];
+                                break;
+                            case 4:
+                                op = "/";
+                                result = arr[1] / arr[2];
+                                break;
+                            default:
+                                op = "INVALID";
+                                break;
+                        }
+
+                        System.out.println("Instruction received = " + arr[1] + " " + op + " " + arr[2]);
+
+                        writer.println(result);
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error handling client");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error starting server");
+        }
+    }
+}
---
+++ client/src/main/java/com/client/Client.java
@@ -0,0 +1,68 @@
+package com.client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.io.IOException;
+
+public class Client {
+
+    private static final int PORT = 5000;
+    private static final String ADDRESS = "calculator";
+
+    public static void main(String[] args) throws IOException {
+
+        try (Socket soc = new Socket(ADDRESS, PORT)) {
+
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+
+            int op, a, b;
+            String msg, opString;
+            while (true) {
+
+                op = generateRandom(4);
+                a = generateRandom(100);
+                b = generateRandom(100);
+                msg = op + " " + a + " " + b;
+
+                switch (op) {
+                    case 1:
+                        opString = "+";
+                        break;
+                    case 2:
+                        opString = "-";
+                        break;
+                    case 3:
+                        opString = "x";
+                        break;
+                    case 4:
+                        opString = "/";
+                        break;
+                    default:
+                        // Never happening
+                        opString = "";
+                        break;
+                }
+
+                out.println(msg);
+                System.out.println("Instruction sent: " + a + " " + opString + " " + b);
+
+                String response = in.readLine();
+                System.out.println("Result = " + response);
+
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    continue;
+                }
+            }
+        }
+    }
+
+    static int generateRandom(int upper) {
+        // Generate random number between 1 and upper
+        return ((int) (Math.random() * upper)) + 1;
+    }
+}
